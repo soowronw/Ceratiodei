196f3098ccac7dd99ff80f93e3b72c38af335609
soowromw <f@f.u>
2020-02-04 11:22:15 +0300

    add db_change_admin.py, rewite run.py

--- config.py
+++ config.py
@@ -2,6 +2,5 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'ceratiodei.db')
-SITE = 'https://www.google.ru'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'alabamaluisiana'
--- run.py
+++ run.py
@@ -1,20 +1,5 @@
-from app import appFlask, db
-from app.models import Links, User, generate_password_hash
-import argparse
-import sys
+from app import appFlask
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-apn', '--adminpanel', default='admincera')
-    parser.add_argument('-al', '--adminlogin', default='admin')
-    parser.add_argument('-aps', '--adminpas', default='admin')
-    namespace = parser.parse_args(sys.argv[1:])
-    adminlink = Links.query.filter_by(role=1).first()
-    adminlink.link = namespace.adminpanel
-    adminlink.route = namespace.adminpanel
-    loginpaslink = User.query.filter_by(role=1).first()
-    loginpaslink.username = namespace.adminlogin
-    loginpaslink.password_hash = generate_password_hash(namespace.adminpas)
-    db.session.commit()
     appFlask.run(debug=True)
